a961f878eba8e2452634318cc40782f60cf8d49b
haykkotnajyan27 <hayk.qot@gmail.com>
2021-10-28 00:19:03 +0400

    two tasks

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log(45);
+//Given a number. Print “odd” if the numberis odd and “even” if itʼs even
+//_____1_____//
+
+// function num(a){
+//     if(a % 2 == 0){
+//         return "odd"
+//     }else{
+//         return "even"
+//     }
+// }
+// console.log(num(2));
+
+
+//.2 Given two variables, which are the angles of a triangle. Find the third angle of the triangle. (Sum of the angles of a triangle equals 180 degrees).
+
+// let c
+// let sum = 180
+// function triangle(a,b){
+//     return c = sum - (a + b)
+// }
+// console.log(triangle(30,30));
+
+
+//.3 Given a positive integer. Bring the last digit of the numberto the beginning. Print the new number. If the last digit of the inserted numberis 0, numberremains the same.
+
+
+
+
+
+
+//.4 Check if a numberis a multiple of 3, 5 or 7 and output the appropriate message
+
+// function multiple(a){
+   
+// }
+// console.log(multiple(10));
