c06fab868cd5e4f618e87127718d4b130c396f76
haykkotnajyan27 <hayk.qot@gmail.com>
2021-10-28 19:54:23 +0400

    change script

--- script.js
+++ script.js
@@ -22,7 +22,19 @@
 
 
 //.3 Given a positive integer. Bring the last digit of the numberto the beginning. Print the new number. If the last digit of the inserted numberis 0, numberremains the same.
-
+// function lastNum(a){
+//     if(a % 10 == 0){
+//         return a
+//     }else{
+//         let n = a % 10
+//         a -= n
+//         a /= 10
+//         n += ""
+//         n += a
+//         return Number(n)
+//     }
+// }
+// console.log(lastNum(500));
 
 
 
