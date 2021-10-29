d9a1495c1b8c2c4a799963db1d1586fb0e3c515e
haykkotnajyan27 <hayk.qot@gmail.com>
2021-10-30 02:40:02 +0400

    add two string files

---
+++ task1.js
@@ -0,0 +1,11 @@
+//Given a number. Print “odd” if the numberis odd and “even” if itʼs even
+//_____1_____//
+
+// function num(a){
+//     if(a % 2 == 0){
+//         return "odd"
+//     }else{
+//         return "even"
+//     }
+// }
+// console.log(num(2));
---
+++ task2.js
@@ -0,0 +1,8 @@
+//.2 Given two variables, which are the angles of a triangle. Find the third angle of the triangle. (Sum of the angles of a triangle equals 180 degrees).
+
+// let c
+// let sum = 180
+// function triangle(a,b){
+//     return c = sum - (a + b)
+// }
+// console.log(triangle(30,30));
