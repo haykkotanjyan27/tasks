1d3bd15b46b85a3ab383087a05c69f5eb3925b29
haykkotnajyan27 <hayk.qot@gmail.com>
2021-10-31 21:39:56 +0400

    change js files

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 //.3 Given a positive integer. Bring the last digit of the numberto the beginning. Print the new number. If the last digit of the inserted numberis 0, numberremains the same.
-function lastNum(a){
-    if(a % 10 == 0){
-        return a
-    }else{
-        let n = a % 10
-        a -= n
-        a /= 10
-        n += ""
-        n += a
-        return Number(n)
-    }
-}
-console.log(lastNum(500));
+// function lastNum(a){
+//     if(a % 10 == 0){
+//         return a
+//     }else{
+//         let n = a % 10
+//         a -= n
+//         a /= 10
+//         n += ""
+//         n += a
+//         return Number(n)
+//     }
+// }
+// console.log(lastNum(500));
 
-let a=488
+
 
 
 
 //.4 Check if a numberis a multiple of 3, 5 or 7 and output the appropriate message
 
-function multiple(a){
-   if(a % 3 == 0 || a % 5 == 0 || a % 7 == 0){
-       return "multiple"
-   }else{
-       return "It is`nt multiple"
-   }
-}
-console.log(multiple(19));
+// function number(res){
+//     let a = 3,
+//         b = 5,
+//         c = 7
+//         if(res % a == 0 || res % b == 0 || res % c == 0){
+//             return `${res} is a multiple`
+//         }else{
+//             return `${res} is not multiple ${a} ${b} ${c}`
+//         }
+// }
+// console.log(number(15));
 
 
